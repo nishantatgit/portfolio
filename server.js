b5a381380712231a5d39ee1b6062b6ff615d3531
nishantatgit <nishant.kumar.shreevastav@gmail.com>
2022-04-03 12:28:08 +0530

    added css file

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const config = {
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+	res.send('index.html');
+});
 app.listen(config.PORT, (req, res) => {
 	console.log('server running at:  ' + config.PORT);
 });
