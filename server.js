db8c230a8e90c9ce6644d6d9fa4f12d1b75c5095
nishantatgit <nishant.kumar.shreevastav@gmail.com>
2022-04-05 09:24:49 +0530

    made changes to server.js added absolute path for public

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const config = {
 	PORT: 4001
 }
+
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
 	res.send('index.html');
 });
